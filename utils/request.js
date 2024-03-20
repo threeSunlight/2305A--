3703acada0d955c2dbda9a40a9811ac4d64515dd
wbj <709965309@qq.com>
2024-03-20 09:50:41 +0800

    上传

--- utils/request.js
+++ utils/request.js
@@ -21,6 +21,7 @@
  * 传true需要配置专属域名,false就不需要配置专属域名
 */
  const SubDomain = require('../config')
+ // 在公司中,只需要修改needSubDomain,是否添加,其他的都不用动
  const request = (url, methods,data="", needSubDomain=true) => {
    let result = needSubDomain ? SubDomain.subDomain : ''
    let _url = SubDomain.baseUrl  + result  + url;
@@ -46,6 +47,35 @@
         })
     })
  }
+ //封装finaly方法
+ /**
+ * 小程序的promise没有finally方法，自己扩展下
+ */
+Promise.prototype.finally = function (callback) {
+  var Promise = this.constructor;
+  return this.then(
+    function (value) {
+      Promise.resolve(callback()).then(
+        function () {
+          return value;
+        }
+      );
+    },
+    function (reason) {
+      Promise.resolve(callback()).then(
+        function () {
+          throw reason;
+        }
+      );
+    }
+  );
+}
 
 // 导出
 module.exports = request
+
+/***
+ * 总结: 封装request
+ * 1. 使用new Promise函数封装wx.request方法,封装了url,methods,data,timeout超时时间,以及请求头请求参数的封装,我们又封装了成功回调和失败回调,定制专属域名
+ * 2. 我们又在promise的原型上扩展了一个finally函数,无论成功还是失败就进行回调
+ */
